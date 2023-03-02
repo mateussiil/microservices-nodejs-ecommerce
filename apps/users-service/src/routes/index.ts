9c554f36f21ac5619f7f2698ee1679ce817d9aca
Mateus Silva <mateus_silva97@hotmail.com>
2023-03-01 21:18:51 -0300

    feat(users-service): added users seed &

--- apps/users-service/src/routes/index.ts
+++ apps/users-service/src/routes/index.ts
@@ -1,16 +1,18 @@
 
 import { prisma } from '../utils';
 import { Router } from 'express';
-import { Kafka } from 'kafkajs';
+import { Kafka, Partitioners } from 'kafkajs';
 
 const router = Router();
 
 const kafka = new Kafka({
-  clientId: 'my-app',
-  brokers: ['localhost:9092']
+  clientId: 'users-service',
+  brokers: ['kafka:9092', 'localhost:9092']
 })
 
 const producer = kafka.producer()
+
+
 
 router.get('/', (req, res) => {
   return res.json({ id: 1 })
@@ -20,6 +22,10 @@ router.post('/register', async (req, res) => {
   const { name, email } = req.body;
 
   try {
+    const admin = kafka.admin()
+
+    await admin.connect()
+
     await producer.connect()
 
     await producer.send({
@@ -30,10 +36,10 @@ router.post('/register', async (req, res) => {
     })
     await producer.disconnect()
 
-    res.status(201).send({ id: 1 })
+    return res.status(201).send({ id: 1 })
   } catch (error) {
     console.error(`Error sending message to Kafka: ${error}`)
-    res.status(500).send({ message: 'Error creating user' })
+    return res.status(500).send({ message: 'Error creating user' })
   }
 });
 
--- apps/users-service/src/utils/prisma.ts
+++ apps/users-service/src/utils/prisma.ts
@@ -1,12 +1,14 @@
 import { PrismaClient } from '@prisma/client';
 
 declare global {
+  //@ts-ignore
   const prisma: PrismaClient | undefined;
 }
-
+//@ts-ignore
 // https://www.prisma.io/docs/support/help-articles/nextjs-prisma-client-dev-practices
 const prisma = global.prisma || new PrismaClient();
 
+//@ts-ignore
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
 
 export default prisma;
