a907e9cc18749bbc16d6724ea4c3236aa2f9cad1
Mateus Silva <mateus_silva97@hotmail.com>
2023-03-05 09:58:19 -0300

    refactor: removed price-service

--- apps/prices-services/src/utils/common.ts
+++
@@ -1,6 +0,0 @@
-export const timestampToDate = (t?: number) => {
-  if (!t) {
-    return null;
-  }
-  return new Date(t * 1000);
-};
--- apps/prices-services/src/utils/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as prisma } from './prisma';
-export { default as getStripe } from './getStripe';
-export { default as stripe } from './stripe';
